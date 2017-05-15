3330df0c6630abd4bc6ae25206cf28cc553d854a
Jarmo Koivisto <jarmo.kvst@gmail.com>
2017-05-15 21:22:56 +0300

    Gradient color

--- ColorUtil.js
+++ ColorUtil.js
@@ -2,7 +2,8 @@
 module.exports = {
     rgbToDec: rgbToDec,
     decToRgbObject: decToRgbObject,
-    decAvgBrightness: decAvgBrightness
+    decAvgBrightness: decAvgBrightness,
+    getGradientColor: getGradientColor
 }
 
 function rgbToDec(r, g, b) {
@@ -25,10 +26,57 @@ function decAvgBrightness(dec) {
     ) / 0x2fd;
 }
 
-    // randomColor: function() {
+function randomColor() {
+    return Math.round(Math.random() * 0xFFFFFF);
+}
 
-    //     return Math.round(Math.random() * 0xFFFFFF);
-    // }
+    /**
+     * @param Gradient color1
+     * @param Gradient color2
+     * @param value Number value from 0 to 100
+     * @return Color between two values.
+     */
+    function getGetColorFromGradient(color1, color2, value) {
+        value = value<0?0:value>100?100:value;
+        var rgb1 = decToRgbObject(color1);
+        var rgb2 = decToRgbObject(color2);
+
+        var scale = {
+            r:(rgb1.r-rgb2.r)/100,
+            g:(rgb1.g-rgb2.g)/100,
+            b:(rgb1.b-rgb2.b)/100
+        };
+
+        return (
+            (rgb1.r-value*scale.r) << 16 |
+            (rgb1.g-value*scale.g) <<8 |
+            (rgb1.b-value*scale.b)
+        );
+    }
+
+    /**
+     * @param colors Several color values
+     * @param value Number value
+     * @param min Minimum of the value
+     * @param max Maximum of the value
+     * @return Color from the colors gradient array at the position indicated with value parameter.
+     */
+    function getGradientColor(colors, value)
+    {
+        if(colors.length === 1) {
+            return colors[0];
+        }
+
+        var min = 0;
+        var max = 1;
+        var v = value < min ? min : value > max ? max : value;
+        var s = max-min;
+        var l = colors.length-1;
+        var i = Math.floor(v/s*l);// index of the color
+        var pl = s/l;// length of one part
+        var nv = (v%pl)/pl*100;// new value. the value between two color values and scale is 0-100
+        return getGetColorFromGradient(colors[i],colors[i+1]?colors[i+1]:colors[i],nv);
+    }
 
     // /**
     //  * @param Gradient color1
--- ColorUtil.spec.js
+++ ColorUtil.spec.js
@@ -33,9 +33,45 @@ describe("ColorUtil", () => {
         });
     });
 
-    describe("randomColor", () => {
+    describe("getGradientColor", () => {
 
-        it("should convert correctly", () => {
+        it("should get color from 2 point gradient", () => {
+            ColorUtil.getGradientColor([0x00FF7F, 0xFF00FF], 0)
+                .should.equal(0x00FF7F);
+            ColorUtil.getGradientColor([0x00FF7F, 0xFF00FF], 1)
+                .should.equal(0xFF00FF);
+            ColorUtil.getGradientColor([0x00FF7F, 0xFF00FF], 0.25)
+                .should.equal(0x3FBF9F);
+            ColorUtil.getGradientColor([0x00FF7F, 0xFF00FF], 0.5)
+                .should.equal(0x7F7FBF);
+            ColorUtil.getGradientColor([0x00FF7F, 0xFF00FF], 0.75)
+                .should.equal(0xBF3FDF);
+        });
+
+        it("should get color from 3 point gradient", () => {
+            ColorUtil.getGradientColor([0x000000, 0x7F7F7F, 0xFFFFFF], 0)
+                .should.equal(0x0);
+            ColorUtil.getGradientColor([0x000000, 0x7F7F7F, 0xFFFFFF], 1)
+                .should.equal(0xFFFFFF);
+            ColorUtil.getGradientColor([0x000000, 0x7F7F7F, 0xFFFFFF], 0.25)
+                .should.equal(0x3F3F3F);
+            ColorUtil.getGradientColor([0x000000, 0x7F7F7F, 0xFFFFFF], 0.5)
+                .should.equal(0x7F7F7F);
+            ColorUtil.getGradientColor([0x000000, 0x7F7F7F, 0xFFFFFF], 0.75)
+                .should.equal(0xBFBFBF);
+        });
+
+        it("should get color from 4 point gradient", () => {
+            ColorUtil.getGradientColor([0xFFFFFF, 0x0, 0x0, 0x0], 0)
+                .should.equal(0xFFFFFF); // first color
+            ColorUtil.getGradientColor([0x0, 0x0, 0x0, 0xFFFFFF], 1)
+                .should.equal(0xFFFFFF); // last color
+            ColorUtil.getGradientColor([0x0, 0xFFFFFF, 0x7F7F7F, 0x7F7F7F], 0.25)
+                .should.equal(0xBFBFBF); // 75% between colors 0 and 1
+            ColorUtil.getGradientColor([0x0, 0xFFFFFF, 0x7F7F7F, 0x0], 0.5)
+                .should.equal(0xBFBFBF); // 50% beween colors 1 and 2
+            ColorUtil.getGradientColor([0x0, 0x0, 0x7F7F7F, 0xFFFFFF], 0.75)
+                .should.equal(0x9F9F9F); // 25% between colors 2 and 3
         });
     });
 });
