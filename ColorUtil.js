5a1b98f1e616763f2c0f56991b14c813f1a86fdc
Jarmo Koivisto <jarmo.kvst@gmail.com>
2017-05-30 23:00:28 +0300

    Conversion functions

--- ColorUtil.js
+++ ColorUtil.js
@@ -1,142 +1,202 @@
+'use strict';
 
 module.exports = {
-    rgbToDec: rgbToDec,
-    decToRgbObject: decToRgbObject,
-    decAvgBrightness: decAvgBrightness,
-    getGradientColor: getGradientColor
+    obj: {
+        toDec: objToDec,
+        toHex: objToHex,
+        toRgba: objToRgba
+    },
+    dec: {
+        toObj: decToObj,
+        toHex: decToHex,
+        toRgba: decToRgba
+    },
+    hex: {
+        toObj: hexToObj,
+        toDec: hexToDec,
+        toRgba: hexToRgba
+    },
+    rgba: {
+
+    },
+    // objToDec: objToDec,
+    // decToRgbObject: decToRgbObject,
+    // decAvgBrightness: decAvgBrightness,
+    // getGradientColor: getGradientColor,
+    // decBrightness: decBrightness,
+    // randomColor: randomColor
 }
 
-function rgbToDec(r, g, b) {
-    return (r<<16 | g<<8 | b);
+function objToDec(o) {
+    return o.r << 16 | o.g << 8 | o.b;
 }
 
-function decToRgbObject(dec) {
+function objToHex(o) {
+    // e.g. (10<<8).toString(16) equals A00, but we need write this in format 0A00
+    // by adding 1<<16 (10000) to the result and removing the first digit
+    // we have produced 0A00 like this: ((1<<16) + (10<<8)).toString(16).slice(1)
+    return '#' + ((1 << 24) + (o.r << 16) + (o.g << 8) + o.b).toString(16).slice(1);
+}
+
+function objToRgba(o, opacity) {
+    opacity = !isNaN(parseFloat(opacity)) ? opacity : 1;
+    return 'rgba('+o.r+','+o.g+','+o.b+','+opacity+')';
+}
+
+function decToObj(dec) {
     return {
-        r: (dec & 0xff0000) >> 16,
-        g: (dec & 0x00ff00) >> 8,
-        b: dec & 0x0000ff
+        r: (dec & 0xFF0000) >> 16,
+        g: (dec & 0x00FF00) >> 8,
+        b: dec & 0x0000FF
     };
 }
 
-function decAvgBrightness(dec) {
-    return (
-        ((dec & 0xff0000) >> 16) +
-        ((dec & 0x00ff00) >> 8) +
-        (dec & 0x0000ff)
-    ) / 0x2fd;
+function decToHex(dec) {
+    return '#' + ((1 << 24) + dec).toString(16).slice(1);
 }
 
-function randomColor() {
-    return Math.round(Math.random() * 0xFFFFFF);
+function decToRgba(dec, opacity) {
+    opacity = !isNaN(parseFloat(opacity)) ? opacity : 1;
+    return 'rgba('
+            + ((dec & 0xFF0000) >> 16) + ','
+            + ((dec & 0x00FF00) >> 8) + ','
+            + (dec & 0x0000FF) + ','
+            + opacity +')';
 }
 
-    /**
-     * @param Gradient color1
-     * @param Gradient color2
-     * @param value Number value from 0 to 100
-     * @return Color between two values.
-     */
-    function getGetColorFromGradient(color1, color2, value) {
-        value = value<0?0:value>100?100:value;
-        var rgb1 = decToRgbObject(color1);
-        var rgb2 = decToRgbObject(color2);
+function hexToObj(hex) {
+    hex = hex.replace(/^#?([a-f\d])([a-f\d])([a-f\d])$/i, function(m, r, g, b) {
+        return r + r + g + g + b + b;
+    });
 
-        var scale = {
-            r:(rgb1.r-rgb2.r)/100,
-            g:(rgb1.g-rgb2.g)/100,
-            b:(rgb1.b-rgb2.b)/100
-        };
+    var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
 
-        return (
-            (rgb1.r-value*scale.r) << 16 |
-            (rgb1.g-value*scale.g) <<8 |
-            (rgb1.b-value*scale.b)
-        );
-    }
+    return result ? {
+        r: parseInt(result[1], 16),
+        g: parseInt(result[2], 16),
+        b: parseInt(result[3], 16)
+    } : null;
+}
 
-    /**
-     * @param colors Several color values
-     * @param value Number value
-     * @param min Minimum of the value
-     * @param max Maximum of the value
-     * @return Color from the colors gradient array at the position indicated with value parameter.
-     */
-    function getGradientColor(colors, value)
-    {
-        if(colors.length === 1) {
-            return colors[0];
-        }
+function hexToDec(hex) {
+    return parseInt(
+        hex.replace(/^#?([a-f\d])([a-f\d])([a-f\d])$/i, function(m, r, g, b) {
+            return r + r + g + g + b + b;
+        })
+        .replace('#', ''), 16);
+}
 
-        var min = 0;
-        var max = 1;
-        var v = value < min ? min : value > max ? max : value;
-        var s = max-min;
-        var l = colors.length-1;
-        var i = Math.floor(v/s*l);// index of the color
-        var pl = s/l;// length of one part
-        var nv = (v%pl)/pl*100;// new value. the value between two color values and scale is 0-100
-        return getGetColorFromGradient(colors[i],colors[i+1]?colors[i+1]:colors[i],nv);
-    }
+function hexToRgba(hex, opacity) {
+    opacity = !isNaN(parseFloat(opacity)) ? opacity : 1;
+    hex = hex.replace(/^#?([a-f\d])([a-f\d])([a-f\d])$/i, function(m, r, g, b) {
+        return r + r + g + g + b + b;
+    });
 
-    // /**
-    //  * @param Gradient color1
-    //  * @param Gradient color2
-    //  * @param value Number value from 0 to 100
-    //  * @return Color between two values.
-    //  */
-    // public static function getGetColorFromGradient(color1:uint, color2:uint, value:Number):uint
-    // {
-    //     value = value<0?0:value>100?100:value;
-    //     var rgb1:Object = hexToRgb(color1);
-    //     var rgb2:Object = hexToRgb(color2);
-    //     var scale:Object = {r:(rgb1.r-rgb2.r)/100, g:(rgb1.g-rgb2.g)/100, b:(rgb1.b-rgb2.b)/100};
-    //     return ((rgb1.r-value*scale.r)<<16|(rgb1.g-value*scale.g)<<8|(rgb1.b-value*scale.b));
-    // }
+    var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
 
-    // /**
-    //  * @param colors Several color values
-    //  * @param value Number value
-    //  * @param min Minimum of the value
-    //  * @param max Maximum of the value
-    //  * @return Color from the colors gradient array at the position indicated with value parameter.
-    //  */
-    // public static function getColorAtValue(colors:Array, value:Number, min:int=0, max:int=100):uint
-    // {
-    //     if(colors.length==1) return colors[0];
-    //     var v:int = (value<min?min:value>max?max:value)-min;
-    //     var s:int = max-min;
-    //     var l:int = colors.length-1;
-    //     var i:int = Math.floor(v/s*l);// index of the color
-    //     var pl:Number = s/l;// length of one part
-    //     var nv:Number = (v%pl)/pl*100;// new value. the value between two color values and scale is 0-100
-    //     return getGetColorFromGradient(colors[i],colors[i+1]?colors[i+1]:colors[i],nv);
-    // }
+    return result ? 'rgba('
+        + parseInt(result[1], 16) + ','
+        + parseInt(result[2], 16) + ','
+        + parseInt(result[3], 16) + ','
+        + opacity + ')'
+    : null;
+}
+
+// /**
+//  * Brightness is determined by average of all color components
+//  *
+//  * @param      {number}  dec
+//  * @return     {number}  number from 0 to 1
+//  */
+// function decAvgBrightness(dec) {
+//     return (
+//         ((dec & 0xFF0000) >> 16) +
+//         ((dec & 0x00FF00) >> 8) +
+//         (dec & 0x0000FF)
+//     ) / 0x2FD;
+// }
+
+// /**
+//  * Brightness is determined by the color component having highest value
+//  *
+//  * @param      {number}  dec
+//  * @return     {number}  number from 0 to 1
+//  */
+// function decBrightness(dec) {
+//     var r = (dec & 0xFF0000) >> 16;
+//     var g = (dec & 0x00FF00) >> 8;
+//     var b = dec & 0x0000FF;
+
+//     return Math.max(r, g, b) / 0xFF;
+// }
+
+// function randomColor() {
+//     return (Math.random() * 0xFFFFFF) | 0;
+// }
+
+//     /**
+//      * @param Gradient color1
+//      * @param Gradient color2
+//      * @param value Number value from 0 to 100
+//      * @return Color between two values.
+//      */
+//     function getGetColorFromGradient(color1, color2, value) {
+//         value = value<0?0:value>100?100:value;
+//         var rgb1 = decToRgbObject(color1);
+//         var rgb2 = decToRgbObject(color2);
+
+//         var scale = {
+//             r:(rgb1.r-rgb2.r)/100,
+//             g:(rgb1.g-rgb2.g)/100,
+//             b:(rgb1.b-rgb2.b)/100
+//         };
+
+//         return (
+//             (rgb1.r-value*scale.r) << 16 |
+//             (rgb1.g-value*scale.g) <<8 |
+//             (rgb1.b-value*scale.b)
+//         );
+//     }
+
+//     /**
+//      * @param colors Several color values
+//      * @param value Number value
+//      * @param min Minimum of the value
+//      * @param max Maximum of the value
+//      * @return Color from the colors gradient array at the position indicated with value parameter.
+//      */
+//     function getGradientColor(colors, value)
+//     {
+//         if (colors.length === 1) {
+//             return colors[0];
+//         }
+
+//         var min = 0;
+//         var max = 1;
+//         var v = value < min ? min : value > max ? max : value;
+//         var s = max-min;
+//         var l = colors.length-1;
+//         var i = Math.floor(v/s*l);// index of the color
+//         var pl = s/l;// length of one part
+//         var nv = (v%pl)/pl*100;// new value. the value between two color values and scale is 0-100
+//         return getGetColorFromGradient(colors[i],colors[i+1]?colors[i+1]:colors[i],nv);
+//     }
 
     // /**
     //  * Returns a random color value that is not too dark and not too bright
+    //  *
+    //  * @param {number} minBrightness
+    //  * @param {number} maxBrightness
     //  */
-    // public static function getRandomColorFromBrightnessRange(minBrightness:uint=40, maxBrightness:uint=70):uint
+    // function getRandomColorFromBrightnessRange(minBrightness, maxBrightness)
     // {
-    //     var color:uint = getRandomColor();
-    //     var brightness:int = getBrightness( color );
+    //     var color = (Math.random() * 0xFFFFFF) | 0;
+    //     var brightness = getBrightness( color );
     //     if( brightness < minBrightness )
     //         color = brightenColor(color,minBrightness);
     //     else if( brightness > maxBrightness )
     //         color = darkenColor(color,maxBrightness);
     //     return color;
-    // }
-
-    // public static function brightness(hex:Number):Number{
-    //     var max:Number=0;
-    //     var rgb:Object=hexToRgb(hex);
-    //     if(rgb.r>max)
-    //         max=rgb.r;
-    //     if(rgb.g>max)
-    //         max=rgb.g;
-    //     if(rgb.b>max)
-    //         max=rgb.b;
-    //     max/=255;
-    //     return max;
     // }
 
     // public static function brightenColor(hexColor:Number, percent:Number):Number {
--- ColorUtil.spec.js
+++ ColorUtil.spec.js
@@ -1,77 +1,183 @@
 
-require("chai").should();
+require('chai').should();
+var expect = require('chai').expect;
 
-var ColorUtil = require("./ColorUtil");
+var C = require('./ColorUtil');
 
-describe("ColorUtil", () => {
+describe('ColorUtil', () => {
 
-    describe("rgbToDec", () => {
+    describe('conversion', () => {
 
-        it("should convert correctly", () => {
-            ColorUtil.rgbToDec(171, 205, 239).should.equal(11259375);
+        var dec = 11189196;
+        var obj = {r: 170, g: 187, b: 204};
+        var hex = '#aabbcc';
+        var rgba = 'rgba(170,187,204,1)';
+
+        describe('obj', () => {
+
+            it('toDec', () => {
+                C.obj.toDec(obj).should.equal(dec);
+            });
+
+            it('toHex', () => {
+                C.obj.toHex(obj).should.equal(hex);
+                C.obj.toHex({r: 0, g: 187, b: 0}).should.equal('#00bb00');
+            });
+
+            it('toRgba', () => {
+                C.obj.toRgba(obj).should.equal(rgba);
+                C.obj.toRgba(obj, 0).should.equal('rgba(170,187,204,0)');
+                C.obj.toRgba(obj, 0.1).should.equal('rgba(170,187,204,0.1)');
+                C.obj.toRgba(obj, 0/0).should.equal(rgba);
+            });
+        });
+
+        describe('dec', () => {
+
+            it('toObj', () => {
+                C.dec.toObj(dec).should.eql(obj);
+            });
+
+            it('toHex', () => {
+                C.dec.toHex(dec).should.equal(hex);
+                C.dec.toHex(0x00bb00).should.equal('#00bb00');
+            });
+
+            it('toRgba', () => {
+                C.dec.toRgba(dec).should.equal(rgba);
+                C.dec.toRgba(dec, 0).should.equal('rgba(170,187,204,0)');
+                C.dec.toRgba(dec, 0.1).should.equal('rgba(170,187,204,0.1)');
+                C.dec.toRgba(dec, 0/0).should.equal(rgba);
+            });
+        });
+
+        describe('hex', () => {
+
+            it('toObj', () => {
+                C.hex.toObj(hex).should.eql(obj);
+                C.hex.toObj('#abc').should.eql(obj);
+                C.hex.toObj('abc').should.eql(obj);
+                C.hex.toObj('aabbcc').should.eql(obj);
+                expect(C.hex.toObj('#abcc')).to.be.null;
+            });
+
+            it('toDec', () => {
+                C.hex.toDec(hex).should.equal(dec);
+                C.hex.toDec('aabbcc').should.equal(dec);
+                C.hex.toDec('#abc').should.equal(dec);
+                C.hex.toDec('abc').should.equal(dec);
+                C.hex.toDec('#112233').should.equal(0x112233);
+            });
+
+            it('toRgba', () => {
+                C.hex.toRgba(hex).should.equal(rgba);
+                C.hex.toRgba(hex, 0).should.equal('rgba(170,187,204,0)');
+                C.hex.toRgba(hex, 0.1).should.equal('rgba(170,187,204,0.1)');
+                C.hex.toRgba(hex, 0/0).should.equal(rgba);
+                C.hex.toRgba('aabbcc').should.equal(rgba);
+                C.hex.toRgba('#abc').should.equal(rgba);
+                C.hex.toRgba('abc').should.equal(rgba);
+                C.hex.toRgba('112233').should.equal('rgba(17,34,51,1)');
+            });
+        });
+
+        describe('rgba', () => {
+
+            xit('toObj', () => {
+                // C.rgba.toObj(rgba).should.eql(obj);
+            });
+
+            xit('toDec', () => {
+                // C.rgba.toDec(rgba).should.equal(hex);
+                // C.rgba.toDec(0x00bb00).should.equal('#00bb00');
+            });
+
+            xit('toHex', () => {
+                // C.rgba.toRgba(rgba).should.equal(rgba);
+                // C.rgba.toRgba(rgba, 0).should.equal('rgba(170,187,204,0)');
+                // C.rgba.toRgba(rgba, 0.1).should.equal('rgba(170,187,204,0.1)');
+                // C.rgba.toRgba(rgba, 0/0).should.equal(rgba);
+            });
         });
     });
 
-    describe("decToRgbObject", () => {
+    // describe('rgbToDec', () => {
 
-        it("should convert correctly", () => {
-            var color = ColorUtil.decToRgbObject(11259375);
+    //     it('should convert correctly', () => {
+    //         ColorUtil.rgbToDec(171, 205, 239).should.equal(11259375);
+    //     });
+    // });
 
-            color.r.should.equal(171);
-            color.g.should.equal(205);
-            color.b.should.equal(239);
-        });
-    });
+    // describe('decToRgbObject', () => {
 
-    describe("decAvgBrightness", () => {
+    //     it('should convert correctly', () => {
+    //         var color = ColorUtil.decToRgbObject(11259375);
 
-        it("should convert correctly", () => {
-            ColorUtil.decAvgBrightness(0xffffff).should.equal(1);
-            ColorUtil.decAvgBrightness(0x0).should.equal(0);
-            ColorUtil.decAvgBrightness(0x00ff00).should.be.closeTo(0.333, 0.01);
-            ColorUtil.decAvgBrightness(0x7f007f).should.be.closeTo(0.333, 0.01);
-        });
-    });
+    //         color.r.should.equal(171);
+    //         color.g.should.equal(205);
+    //         color.b.should.equal(239);
+    //     });
+    // });
 
-    describe("getGradientColor", () => {
+    // describe('decAvgBrightness', () => {
 
-        it("should get color from 2 point gradient", () => {
-            ColorUtil.getGradientColor([0x00FF7F, 0xFF00FF], 0)
-                .should.equal(0x00FF7F);
-            ColorUtil.getGradientColor([0x00FF7F, 0xFF00FF], 1)
-                .should.equal(0xFF00FF);
-            ColorUtil.getGradientColor([0x00FF7F, 0xFF00FF], 0.25)
-                .should.equal(0x3FBF9F);
-            ColorUtil.getGradientColor([0x00FF7F, 0xFF00FF], 0.5)
-                .should.equal(0x7F7FBF);
-            ColorUtil.getGradientColor([0x00FF7F, 0xFF00FF], 0.75)
-                .should.equal(0xBF3FDF);
-        });
+    //     it('should convert correctly', () => {
+    //         ColorUtil.decAvgBrightness(0xffffff).should.equal(1);
+    //         ColorUtil.decAvgBrightness(0x0).should.equal(0);
+    //         ColorUtil.decAvgBrightness(0x00ff00).should.be.closeTo(0.333, 0.01);
+    //         ColorUtil.decAvgBrightness(0x7f007f).should.be.closeTo(0.333, 0.01);
+    //     });
+    // });
 
-        it("should get color from 3 point gradient", () => {
-            ColorUtil.getGradientColor([0x000000, 0x7F7F7F, 0xFFFFFF], 0)
-                .should.equal(0x0);
-            ColorUtil.getGradientColor([0x000000, 0x7F7F7F, 0xFFFFFF], 1)
-                .should.equal(0xFFFFFF);
-            ColorUtil.getGradientColor([0x000000, 0x7F7F7F, 0xFFFFFF], 0.25)
-                .should.equal(0x3F3F3F);
-            ColorUtil.getGradientColor([0x000000, 0x7F7F7F, 0xFFFFFF], 0.5)
-                .should.equal(0x7F7F7F);
-            ColorUtil.getGradientColor([0x000000, 0x7F7F7F, 0xFFFFFF], 0.75)
-                .should.equal(0xBFBFBF);
-        });
+    // describe('decBrightness', () => {
 
-        it("should get color from 4 point gradient", () => {
-            ColorUtil.getGradientColor([0xFFFFFF, 0x0, 0x0, 0x0], 0)
-                .should.equal(0xFFFFFF); // first color
-            ColorUtil.getGradientColor([0x0, 0x0, 0x0, 0xFFFFFF], 1)
-                .should.equal(0xFFFFFF); // last color
-            ColorUtil.getGradientColor([0x0, 0xFFFFFF, 0x7F7F7F, 0x7F7F7F], 0.25)
-                .should.equal(0xBFBFBF); // 75% between colors 0 and 1
-            ColorUtil.getGradientColor([0x0, 0xFFFFFF, 0x7F7F7F, 0x0], 0.5)
-                .should.equal(0xBFBFBF); // 50% beween colors 1 and 2
-            ColorUtil.getGradientColor([0x0, 0x0, 0x7F7F7F, 0xFFFFFF], 0.75)
-                .should.equal(0x9F9F9F); // 25% between colors 2 and 3
-        });
-    });
+    //     it('should convert correctly', () => {
+    //         ColorUtil.decBrightness(0xffffff).should.equal(1);
+    //         ColorUtil.decBrightness(0x0).should.equal(0);
+    //         ColorUtil.decBrightness(0x00ff00).should.equal(1);
+    //         ColorUtil.decBrightness(0x7f007f).should.be.closeTo(0.5, 0.01);
+    //     });
+    // });
+
+    // describe('getGradientColor', () => {
+
+    //     it('should get color from 2 point gradient', () => {
+    //         ColorUtil.getGradientColor([0x00FF7F, 0xFF00FF], 0)
+    //             .should.equal(0x00FF7F);
+    //         ColorUtil.getGradientColor([0x00FF7F, 0xFF00FF], 1)
+    //             .should.equal(0xFF00FF);
+    //         ColorUtil.getGradientColor([0x00FF7F, 0xFF00FF], 0.25)
+    //             .should.equal(0x3FBF9F);
+    //         ColorUtil.getGradientColor([0x00FF7F, 0xFF00FF], 0.5)
+    //             .should.equal(0x7F7FBF);
+    //         ColorUtil.getGradientColor([0x00FF7F, 0xFF00FF], 0.75)
+    //             .should.equal(0xBF3FDF);
+    //     });
+
+    //     it('should get color from 3 point gradient', () => {
+    //         ColorUtil.getGradientColor([0x000000, 0x7F7F7F, 0xFFFFFF], 0)
+    //             .should.equal(0x0);
+    //         ColorUtil.getGradientColor([0x000000, 0x7F7F7F, 0xFFFFFF], 1)
+    //             .should.equal(0xFFFFFF);
+    //         ColorUtil.getGradientColor([0x000000, 0x7F7F7F, 0xFFFFFF], 0.25)
+    //             .should.equal(0x3F3F3F);
+    //         ColorUtil.getGradientColor([0x000000, 0x7F7F7F, 0xFFFFFF], 0.5)
+    //             .should.equal(0x7F7F7F);
+    //         ColorUtil.getGradientColor([0x000000, 0x7F7F7F, 0xFFFFFF], 0.75)
+    //             .should.equal(0xBFBFBF);
+    //     });
+
+    //     it('should get color from 4 point gradient', () => {
+    //         ColorUtil.getGradientColor([0xFFFFFF, 0x0, 0x0, 0x0], 0)
+    //             .should.equal(0xFFFFFF); // first color
+    //         ColorUtil.getGradientColor([0x0, 0x0, 0x0, 0xFFFFFF], 1)
+    //             .should.equal(0xFFFFFF); // last color
+    //         ColorUtil.getGradientColor([0x0, 0xFFFFFF, 0x7F7F7F, 0x7F7F7F], 0.25)
+    //             .should.equal(0xBFBFBF); // 75% between colors 0 and 1
+    //         ColorUtil.getGradientColor([0x0, 0xFFFFFF, 0x7F7F7F, 0x0], 0.5)
+    //             .should.equal(0xBFBFBF); // 50% beween colors 1 and 2
+    //         ColorUtil.getGradientColor([0x0, 0x0, 0x7F7F7F, 0xFFFFFF], 0.75)
+    //             .should.equal(0x9F9F9F); // 25% between colors 2 and 3
+    //     });
+    // });
 });
